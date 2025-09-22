596348fe206e1c87293f13827a158d3bad3eeb7f
GemsLister <jameslester.l.lopez@gmail.com>
2025-09-22 17:17:50 +0800

    asdasdas

---
+++ script.js
@@ -0,0 +1,8 @@
+const container4Sec = document.querySelector(".container4Sec");
+const signUpBtn = document.querySelector(".sign-up-btn");
+
+signUpBtn.addEventListener("click", () => {
+  container4Sec.style.cssText = `
+    background-color: "green";
+    border-radius: "13px"`;
+});
